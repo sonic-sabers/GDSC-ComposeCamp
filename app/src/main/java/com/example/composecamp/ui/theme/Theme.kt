6ef7bb5af226e1978e68b2b4f88459048f9f9299
Ashish <sonic-sabers@Ashishs-MacBook-Air.local>
2022-10-20 18:00:36 +0530

    Added Files

---
+++ app/src/main/java/com/example/composecamp/ui/theme/Theme.kt
@@ -0,0 +1,44 @@
+package com.example.composecamp.ui.theme
+
+import android.graphics.Color
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.shapes
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.White
+
+private val DarkColorPalette = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+)
+
+private val LightColorPalette = lightColors(
+    surface = Blue,
+    onSurface = White,
+    primary = LightBlue,
+    onPrimary = Navy
+)
+
+@Composable
+fun ComposeCampTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = typography,
+        shapes = shapes,
+        content = content
+    )
+}
